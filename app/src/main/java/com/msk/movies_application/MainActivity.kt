6860d76350de25e2cb3102a109df6ae898357978
serhatkutlu <84921644+serhatkutlu@users.noreply.github.com>
2021-12-29 13:49:51 +0300

    Initial commit

---
+++ app/src/main/java/com/msk/movies_application/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.msk.movies_application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
